30356b488ce14f5e574ee2a337a82c3dba016e9a
Piotr Waszkiewicz <waszka23@gmail.com>
2016-12-21 01:27:41 +0100

    Changes to slanting tree

--- Program/classifier_tree/slanting_tree.py
+++ Program/classifier_tree/slanting_tree.py
@@ -1,5 +1,6 @@
 import copy
 import numpy as np
+from random import randint
 from balanced_tree import BinaryTree, Node
 from Program.models import classifiers
 
@@ -9,7 +10,7 @@ class SlantingTree(BinaryTree):
     Tree for classifying and rejecting patterns in the shape of a slanted tree.
     """
 
-    def __init__(self, classification_method=("svm", None)):
+    def __init__(self, classification_method=("svm", None), point_generation_method=1):
         """
         Creates SlantingTree with provided classifier.
         Raises a ValueError exception if provided parameters are incorrect.
@@ -18,6 +19,7 @@ class SlantingTree(BinaryTree):
         """
         BinaryTree.__init__(self)
         self.classifier = classifiers.classifier_object(classification_method)
+        self._point_generation_method = point_generation_method
 
     def build(self, labels, patterns):
         # TODO: Add method description
@@ -33,7 +35,7 @@ class SlantingTree(BinaryTree):
             raise ValueError('Tree has not yet been constructed. Root does not exist.')
 
         node = self._root
-        while node.right is not None:
+        while node.right is not None and node.right.left is not None:
             print '[' + str(node.right.classes) + '] -> ' + str(node.right.left.classes)
             node = node.left
         print '[' + str(node.classes) + ']'
@@ -50,28 +52,60 @@ class SlantingTree(BinaryTree):
         return [SlantingTree._classify_pattern(self._root, p) for p in patterns]
 
     def _create_tree_node(self, classes, patterns_by_class):
-        if len(classes) == 1:
-            return self._create_tree_leaf(classes[0], None, patterns_by_class)
         node = Node()
         node.classes = classes
-        node.classes_left = classes[1:]
         node.classes_right = classes[0]
-        node.left = self._create_tree_node(node.classes_left, patterns_by_class)
-        node.right = self._create_tree_leaf(node.classes_right, node.left, patterns_by_class)
+        node.classes_left = classes[1:]
         self._train_classifier(node, node.classes_right, patterns_by_class)
+        node.left = self._create_tree_node(node.classes_left, patterns_by_class) if len(classes) > 1 else None
+        node.right = self._create_tree_leaf(node.classes_right, node, patterns_by_class)
+
         return node
 
-    def _create_tree_leaf(self, class_number, failure_node, patterns_by_class):
+    def _create_tree_leaf(self, class_number, parent_node, patterns_by_class):
         node = Node()
         node.classes = class_number
-        node.left = failure_node
-        node.right = None
-        self._train_classifier(node, node.classes, patterns_by_class)
+        node.left = parent_node.left
+        self._train_classifier_for_leaf(node, node.classes, parent_node, patterns_by_class)
 
         return node
 
     def _train_classifier(self, node, class_number, patterns_by_class):
         node.classifier = copy.deepcopy(self.classifier)
+        native_samples, foreign_samples, labels = SlantingTree._create_training_datasets_and_labels(class_number,
+                                                                                                    patterns_by_class)
+        node.classifier.fit(np.concatenate((native_samples, foreign_samples), axis=0), labels)
+
+    def _train_classifier_for_leaf(self, node, class_number, parent_node, patterns_by_class):
+        parent_classifier = parent_node.classifier
+        node.classifier = copy.deepcopy(self.classifier)
+        native_samples, foreign_samples, labels = SlantingTree._create_training_datasets_and_labels(class_number,
+                                                                                                    patterns_by_class)
+        dataset = np.concatenate((native_samples, foreign_samples), axis=0)
+        results = parent_classifier.predict(dataset)
+        native_samples, foreign_samples, labels = self._create_additional_training_points(dataset, labels, results)
+        node.classifier.fit(np.concatenate((native_samples, foreign_samples), axis=0), labels)
+
+    def _create_additional_training_points(self, dataset, labels, results):
+        # TODO: Check which method should be used when creating new points
+        native_samples, foreign_samples = SlantingTree._get_seed_samples(dataset, labels, results)
+        native_samples, foreign_samples = SlantingTree._generate_new_samples(native_samples, foreign_samples,
+                                                                             sum([1 for l in labels if l == 1]),
+                                                                             self._point_generation_method)
+        native_samples_count, foreign_samples_count = len(native_samples), len(foreign_samples)
+        native_samples, foreign_samples = np.asarray(native_samples), np.asarray(foreign_samples)
+        labels = sum([[1] * native_samples_count, [0] * foreign_samples_count], [])
+
+        return native_samples, foreign_samples, labels
+
+    @staticmethod
+    def _create_training_datasets_and_labels(class_number, patterns_by_class):
+        """
+        Creates equal training native and foreign data sets and their corresponding labels.
+        :param class_number: native class number
+        :param patterns_by_class: dictionary of all patterns with class number as a key
+        :return: native and foreign patterns classes with corresponding labels list
+        """
         native_class_samples_count = len(patterns_by_class[class_number])
         foreign_class_samples_count = native_class_samples_count / (len(patterns_by_class) - 1)
         native_samples = np.asarray(patterns_by_class[class_number])
@@ -79,7 +113,55 @@ class SlantingTree(BinaryTree):
             [patterns_by_class[i][:foreign_class_samples_count] for i in patterns_by_class.keys() if i != class_number],
             []))
         labels = sum([[1] * native_class_samples_count, [0] * foreign_samples.shape[0]], [])
-        node.classifier.fit(np.concatenate((native_samples, foreign_samples), axis=0), labels)
+
+        return native_samples, foreign_samples, labels
+
+    @staticmethod
+    def _get_seed_samples(dataset, labels, results):
+        """
+        Chooses those patterns that can be used in artificial patterns generation.
+        :param dataset: set of data used for previous classifier prediction
+        :param labels: proper classes for each pattern
+        :param results: classes recognized by classifier
+        :return: native and foreign patterns that can be used for generation new samples
+        """
+        native_samples, foreign_samples = [], []
+        for i, p in enumerate(dataset):
+            if results[i] == 0:
+                continue
+            elif labels[i] == 1:
+                native_samples.append(p)
+            else:
+                foreign_samples.append(p)
+
+        return native_samples, foreign_samples
+
+    @staticmethod
+    def _generate_new_samples(native_samples, foreign_samples, proper_class_count, method_number):
+        """
+        Creates new, artificial patterns from correctly classified native ones and wrongly classified foreign ones.
+        There are few approaches towards this problem:
+            1) For randomly selected pattern apply gaussian distortion vector
+        :param native_samples: patterns from native class that were correctly classified
+        :param foreign_samples: patterns from foreign class that were wrongly classified
+        :param proper_class_count: number of patterns in each class needed for classifier training
+        :param method_number: point generation method identifier
+        :return: newly created classes
+        """
+        native_count, foreign_count = len(native_samples), len(foreign_samples)
+        # TODO: What if any of the counts is zero?
+        for i in range(native_count, proper_class_count):
+            native_samples.append(SlantingTree._apply_gaussian_distortion(native_samples[randint(0, native_count - 1)]))
+        for i in range(foreign_count, proper_class_count):
+            foreign_samples.append(
+                SlantingTree._apply_gaussian_distortion(foreign_samples[randint(0, foreign_count - 1)]))
+
+        return native_samples, foreign_samples
+
+    @staticmethod
+    def _apply_gaussian_distortion(pattern):
+        distortion = np.random.normal(loc=0.0, scale=1.0, size=pattern.shape[0])
+        return pattern + distortion
 
     @staticmethod
     def _classify_pattern(node, pattern):
