7c986df27fedb5da83be5eca5009444369f7bd02
Piotr Waszkiewicz <waszka23@gmail.com>
2017-05-24 11:43:18 +0200

    Updated test method

--- Program/geometrical_classifiers/shrinking_figures.py
+++ Program/geometrical_classifiers/shrinking_figures.py
@@ -32,36 +32,9 @@ class ShrinkingFigures(NativeFigures):
         :return: list of tuples containing strict native and foreign accuracies
         """
         original_figure_tolerance = self.figure_tolerance
-        figures = self.figures
-        results = [[] for _ in xrange(len(figures))]
-        training, test = self._create_native_lists()
-        natives = list(training)
-        [natives[i].extend(test[i]) for i in xrange(len(training))]
-
-        for step in xrange(1, steps + 1):
-            [results[i].append(self._get_ratios(i, natives, figure)) for i, figure in enumerate(figures)]
-            figures = [self._shrink_figure(training[i], figure, shrinking_option, step=step)
-                       for i, figure in enumerate(figures)]
-            if shrinking_option is self.ShrinkingOption.TOLERANCE_MANIPULATION:
-                self.figure_tolerance -= 0.02
-
-        self.figure_tolerance = original_figure_tolerance
-
-        return results
-
-    def perform_tests2(self, steps=10, shrinking_option=ShrinkingOption.TOLERANCE_MANIPULATION):
-        """
-        Performs rejection and classification ratios tests on decreasing figure.
-        :param steps: number of shrinking steps to make
-        :param shrinking_option: ShrinkingOption enumerator defining shrinking algorithm
-        :return: list of tuples containing strict native and foreign accuracies
-        """
-        original_figure_tolerance = self.figure_tolerance
         original_figures = self.figures
         results = []
-        training, test = self._create_native_lists()
-        natives = list(training)
-        [natives[i].extend(test[i]) for i in xrange(len(training))]
+        training = self.dataset.get_patterns_by_class()[0]
 
         for step in xrange(1, steps + 1):
             matrix = self.get_confusion_matrix(self.foreign_elements, self.figure_tolerance)
@@ -75,11 +48,6 @@ class ShrinkingFigures(NativeFigures):
         self.figures = original_figures
 
         return results
-
-    def _create_native_lists(self):
-        training, test = self.dataset.get_patterns_by_class()
-        natives_training, natives_test = [training[k] for k in training.keys()], [test[k] for k in test.keys()]
-        return natives_training, natives_test
 
     def _get_ratios(self, native_class, native_elements, figure):
         tolerance = self.figure_tolerance
--- Program/main.py
+++ Program/main.py
@@ -158,9 +158,8 @@ if __name__ == "__main__":
             ellipsoids = ShrinkingFigures(digits, letters, minimum_volume_figure_class=HyperRectangle)
             shrinking_option = ShrinkingFigures.ShrinkingOption.TOLERANCE_MANIPULATION if o == "-7" \
                 else ShrinkingFigures.ShrinkingOption.ELEMENTS_REJECTION
-            results = np.asarray(ellipsoids.perform_tests2(steps=100, shrinking_option=shrinking_option), dtype=float)
-            #for i in xrange(results.shape[0]):
-            filename = "../Results/%s_shrinking_ellipsoids.csv" % o[1]
+            results = np.asarray(ellipsoids.perform_tests(steps=2, shrinking_option=shrinking_option), dtype=float)
+            filename = "../Results/%s_shrinking_rectangles.csv" % o[1]
             np.savetxt(filename, results, delimiter=',', fmt='%f')
 
     pool.close()
