c0719cfc42063da0c90fd3180d0407100ec67b43
Piotr Waszkiewicz <waszka23@gmail.com>
2017-06-06 19:24:47 +0200

    Updated algorithm

--- Program/geometrical_classifiers/shrinking_figures.py
+++ Program/geometrical_classifiers/shrinking_figures.py
@@ -38,18 +38,15 @@ class ShrinkingFigures(NativeFigures):
         filename = "../Results/%s_shrinking_%s.csv" % (
             str(shrinking_option), "ellipsoids" if isinstance(self.figures[0], MVEE) else "rectangles")
         for step in xrange(1, steps + 1):
-            try:
-                print "Performing step %i of %i" % (step, steps)
-                matrix = self.get_confusion_matrix(self.foreign_elements, self.figure_tolerance)
-                results.append(self._get_ratios_from_matrix(matrix))
-                self.figures = [self._shrink_figure(training[i], figure, shrinking_option, step=step)
-                                for i, figure in enumerate(self.figures)]
-                if shrinking_option is self.ShrinkingOption.TOLERANCE_MANIPULATION:
-                    self.figure_tolerance -= 0.02
-                with open(filename, 'a') as f:
-                    f.write("%f, %f, %f, %f, %f, %f\n" % tuple(results[-1]))
-            except Exception:
-                pass
+            print "Performing step %i of %i" % (step, steps)
+            matrix = self.get_confusion_matrix(self.foreign_elements, self.figure_tolerance)
+            results.append(self._get_ratios_from_matrix(matrix))
+            self.figures = [self._shrink_figure(training[i], figure, shrinking_option, step=step)
+                            for i, figure in enumerate(self.figures)]
+            if shrinking_option is self.ShrinkingOption.TOLERANCE_MANIPULATION:
+                self.figure_tolerance -= 0.02
+            with open(filename, 'a') as f:
+                f.write("%f, %f, %f, %f, %f, %f\n" % tuple(results[-1]))
 
         self.figure_tolerance = original_figure_tolerance
         self.figures = original_figures
@@ -71,8 +68,10 @@ class ShrinkingFigures(NativeFigures):
         first_test_row = (len(matrix) - 1) / 2
         correctly_classified_training = sum([matrix[i][i] for i in xrange(first_test_row)])
         correctly_classified_test = sum([matrix[first_test_row + i][i] for i in xrange(first_test_row)])
-        correctly_native_training = sum([matrix[i][j] for i in xrange(first_test_row) for j in xrange(len(matrix[i]) - 1)])
-        correctly_native_test = sum([matrix[first_test_row + i][j] for i in xrange(first_test_row) for j in xrange(len(matrix[i]) - 1)])
+        correctly_native_training = sum(
+            [matrix[i][j] for i in xrange(first_test_row) for j in xrange(len(matrix[i]) - 1)])
+        correctly_native_test = sum(
+            [matrix[first_test_row + i][j] for i in xrange(first_test_row) for j in xrange(len(matrix[i]) - 1)])
         correctly_rejected = matrix[-1][-1]
         all_native_training = len(self.dataset.training_data)
         all_native_test = len(self.dataset.test_data)
@@ -92,10 +91,8 @@ class ShrinkingFigures(NativeFigures):
             pass
         elif shrinking_option is self.ShrinkingOption.ELEMENTS_REJECTION:
             elements_removed = 5
-            distances = [figure.calculate_distances(pattern) for pattern in training]
-            too_big_distance = sorted(distances)[-1 * (elements_removed * step)]
-            fig_cls = self.figure_class
-            new_figure = fig_cls([pattern for i, pattern in enumerate(training) if distances[i] < too_big_distance])
+            sorted_patterns = sorted(training, key=figure.calculate_distances)
+            new_figure = self.figure_class(sorted_patterns[:-1 * step * elements_removed])
         else:
             raise TypeError("Unsupported shrinking_option value")
 
